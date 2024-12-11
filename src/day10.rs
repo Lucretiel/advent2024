ac5fa4b13e31fd1a0aacce1cccc86990dd21eea8
Nathan West <Lucretiel@gmail.com>
2024-12-10 22:09:44 -0500

    Day 10 solution

--- src/day10.rs
+++ src/day10.rs
@@ -1,5 +1,6 @@
-use std::convert::Infallible;
+use std::collections::HashSet;
 
+use gridly::prelude::*;
 use gridly_grids::VecGrid;
 use nom::{
     branch::alt,
@@ -11,8 +12,12 @@ use nom::{
 use nom_supreme::{
     error::ErrorTree, final_parser::final_parser, multi::collect_separated_terminated, ParserExt,
 };
+use thiserror::Error;
 
-use crate::library::ITResult;
+use crate::{
+    express,
+    library::{Definitely, ITResult},
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Height(u8);
@@ -22,6 +27,14 @@ impl Height {
         self.0 == origin.0 + 1
     }
 
+    fn is_start(self) -> bool {
+        self.0 == 0
+    }
+
+    fn is_summit(self) -> bool {
+        self.0 == 9
+    }
+
     fn from_char(c: char) -> Option<Self> {
         c.to_digit(10).map(|n| n as u8).map(Height)
     }
@@ -29,7 +42,7 @@ impl Height {
 
 #[derive(Debug)]
 pub struct Input {
-    raw: VecGrid<Height>,
+    grid: VecGrid<Height>,
 }
 
 fn parse_height(input: &str) -> ITResult<&str, Height> {
@@ -63,8 +76,15 @@ fn parse_rows(input: &str) -> ITResult<&str, Vec<Vec<Height>>> {
     collect_separated_terminated(parse_row, success(()), eof).parse(input)
 }
 
+#[derive(Debug, Error)]
+#[error("input didn't have consistent row lengths")]
+struct DimensionError;
+
 fn parse_input(input: &str) -> ITResult<&str, Input> {
-    parse_rows.map_res_cut(|rows| VecGrid::new_from_rows(rows))
+    parse_rows
+        .map_res_cut(|rows| VecGrid::new_from_rows(rows).ok_or(DimensionError))
+        .map(|grid| Input { grid })
+        .parse(input)
 }
 
 impl TryFrom<&str> for Input {
@@ -75,10 +95,77 @@ impl TryFrom<&str> for Input {
     }
 }
 
-pub fn part1(input: Input) -> anyhow::Result<Infallible> {
-    anyhow::bail!("not implemented yet")
+/// Given a particular `location` and `height``, explore all of the neighbors
+/// of that location. For each neighbor, if it is a valid successor, call
+/// `add_summit`; otherwise, recursively call explore on that location.
+fn explore<T>(
+    grid: &impl Grid<Item = Height>,
+    location: Location,
+    height: Height,
+    state: T,
+    add_summit: &impl Fn(T, Location) -> T,
+) -> T {
+    EACH_DIRECTION
+        .iter()
+        .map(|&step| location + step)
+        .filter_map(|new_location| {
+            grid.get(new_location)
+                .ok()
+                .map(|&new_height| (new_location, new_height))
+        })
+        .filter(|&(_, new_height)| new_height.is_valid_successor_from(height))
+        .fold(state, |state, (location, height)| {
+            if height.is_summit() {
+                add_summit(state, location)
+            } else {
+                explore(grid, location, height, state, add_summit)
+            }
+        })
 }
 
-pub fn part2(input: Input) -> anyhow::Result<Infallible> {
-    anyhow::bail!("not implemented yet")
+/// Solve the puzzle by iterating each start point, using `explore` to explore
+/// those start points, then adding together the outputs from `count_trails`.
+/// For each start point, we use `init_trail` to create some state, pass
+/// `add_summit` to explore to explore with that state, then use `count_trails`
+/// to summarize the exploration results.
+fn solve<T>(
+    input: &Input,
+    init_trail: impl Fn() -> T,
+    add_summit: impl Fn(T, Location) -> T,
+    count_trails: impl Fn(T) -> usize,
+) -> Definitely<usize> {
+    Ok(input
+        .grid
+        .rows()
+        .iter()
+        .flat_map(|row| row.iter_with_locations())
+        .filter(|(_, &height)| height.is_start())
+        .map(move |(location, &height)| {
+            count_trails(explore(
+                &input.grid,
+                location,
+                height,
+                init_trail(),
+                &add_summit,
+            ))
+        })
+        .sum())
+}
+
+pub fn part1(input: Input) -> Definitely<usize> {
+    solve(
+        &input,
+        HashSet::new,
+        |reachable_summits, location| express!(reachable_summits.insert(location)),
+        |reachable_summits| reachable_summits.len(),
+    )
+}
+
+pub fn part2(input: Input) -> Definitely<usize> {
+    solve(
+        &input,
+        || 0,
+        |trail_count, _| trail_count + 1,
+        |trail_count| trail_count,
+    )
 }
