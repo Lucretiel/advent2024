677f180b632b245357f8e3d069d67ef5c597c432
Nathan West <Lucretiel@gmail.com>
2024-12-13 14:28:18 -0500

    Day 12 WIP

---
+++ src/day12.rs
@@ -0,0 +1,62 @@
+use std::{
+    collections::{HashMap, HashSet},
+    convert::Infallible,
+};
+
+use gridly::prelude::*;
+use nom_supreme::{error::ErrorTree, final_parser::final_parser};
+
+use crate::library::{ITResult, IterExt};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Cell(u8);
+
+#[derive(Debug)]
+pub struct Input {
+    map: HashMap<Location, Cell>,
+}
+
+impl TryFrom<&str> for Input {
+    type Error = Infallible;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let map = value
+            .lines()
+            .map(|line| line.trim().as_bytes())
+            .with_rows(Row(0))
+            .flat_map(|(row, line)| {
+                line.iter()
+                    .copied()
+                    .map(Cell)
+                    .with_columns(Column(0))
+                    .map(move |(column, cell)| (row.combine(column), cell))
+            })
+            .collect();
+
+        Ok(Input { map })
+    }
+}
+
+fn explore(
+    territory: &HashMap<Location, Cell>,
+    root: &Location,
+    id: Cell,
+    explored_territory: &mut HashSet<Location>,
+) -> usize {
+    let mut area = 0;
+    let mut perimiter = 0;
+
+    let mut region = HashSet::from([root]);
+}
+
+pub fn part1(input: Input) -> anyhow::Result<Infallible> {
+    let mut unexplored_territory = HashSet::from_iter(input.map.keys());
+
+    let items = unexplored_territory.drain();
+
+    items.unexplored_territory.drain()
+}
+
+pub fn part2(input: Input) -> anyhow::Result<Infallible> {
+    anyhow::bail!("not implemented yet")
+}
