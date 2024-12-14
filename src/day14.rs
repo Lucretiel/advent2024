e8563a41e10e236a66177185357c4dbeee7d6071
Nathan West <Lucretiel@gmail.com>
2024-12-14 01:55:46 -0500

    WIP

--- src/day14.rs
+++ src/day14.rs
@@ -1,8 +1,14 @@
 use std::cmp::Ordering;
 use std::convert::Infallible;
+use std::fmt::Display;
+use std::thread::sleep;
+use std::time::Duration;
 
 use anyhow::Context;
-use enum_map::{Enum, EnumMap};
+use enum_map::Enum;
+use joinery::JoinableIterator;
+use joinery::separators::Newline;
+use lazy_format::lazy_format;
 use nom::Parser;
 use nom::character::complete::{char, digit1, multispace0, space0};
 use nom::combinator::{eof, success};
@@ -11,17 +17,9 @@ use nom_supreme::multi::collect_separated_terminated;
 use nom_supreme::{error::ErrorTree, final_parser::final_parser};
 
 use crate::day13::Vector;
-use crate::library::counter::Counter;
-use crate::library::{Definitely, ITResult};
+use crate::library::ITResult;
+use crate::library::counter::EnumCounter;
 use crate::parser;
-
-const ROOM_HEIGHT: i64 = 7;
-const ROOM_WIDTH: i64 = 11;
-
-const ROOM: Vector = Vector {
-    x: ROOM_WIDTH,
-    y: ROOM_HEIGHT,
-};
 
 fn parse_coord(input: &str) -> ITResult<&str, i64> {
     digit1
@@ -109,20 +107,19 @@ struct Quadrant {
 impl Robot {
     fn take_steps(&mut self, steps: i64, room: &Vector) {
         let total_motion = self.velocity * steps;
-        let adjusted_motion = vector_mod(&total_motion, room);
-        let new_position = self.position + adjusted_motion;
+        let new_position = self.position + total_motion;
         self.position = vector_mod(&new_position, room);
     }
 
-    fn compute_quadrant(&self) -> Option<Quadrant> {
-        let x = compute_zone(self.position.x, ROOM.x)?;
-        let y = compute_zone(self.position.y, ROOM.y)?;
+    fn compute_quadrant(&self, room: &Vector) -> Option<Quadrant> {
+        let x = compute_zone(self.position.x, room.x)?;
+        let y = compute_zone(self.position.y, room.y)?;
 
         Some(Quadrant { x, y })
     }
 }
 
-fn get_int_var(var: &str) -> anyhow::Result<Option<i64>> {
+fn get_env_int(var: &str) -> anyhow::Result<Option<i64>> {
     match std::env::var(var) {
         Err(err) => match err {
             std::env::VarError::NotPresent => Ok(None),
@@ -139,51 +136,100 @@ fn get_int_var(var: &str) -> anyhow::Result<Option<i64>> {
     }
 }
 
-fn get_room() -> anyhow::Result<Vector> {
-    let height = get_int_var("DAY_14_ROOM_HEIGHT").context(
+fn get_env_room() -> anyhow::Result<Option<Vector>> {
+    let height = get_env_int("DAY_14_ROOM_HEIGHT").context(
         "error getting room height from \
          environment variable DAY_14_ROOM_HEIGHT",
     )?;
-    let width = get_int_var("DAY_14_ROOM_WIDTH").context(
+    let width = get_env_int("DAY_14_ROOM_WIDTH").context(
         "error getting room width from \
          environment variable DAY_14_ROOM_WIDTH",
     )?;
 
-    const ROOM_HEIGHT: i64 = 7;
-    const ROOM_WIDTH: i64 = 11;
-
-    const ROOM: Vector = Vector {
-        x: ROOM_WIDTH,
-        y: ROOM_HEIGHT,
-    };
-
     Ok(match (height, width) {
-        (None, None) => ROOM,
-        (Some(height), Some(width)) => Vector {
+        (None, None) => None,
+        (Some(height), Some(width)) => Some(Vector {
             x: width,
             y: height,
-        },
+        }),
         (Some(_), None) => anyhow::bail!("room height was given, but room width was omitted"),
         (None, Some(_)) => anyhow::bail!("room width was given, but room height was omitted"),
     })
 }
 
-pub fn part1(mut input: Input) -> anyhow::Result<usize> {
-    let room = get_room()?;
-
-    let robot_counts: Counter<Quadrant, EnumMap<Quadrant, usize>> = input
-        .robots
-        .iter_mut()
-        .filter_map(|robot| {
-            robot.take_steps(100, &room);
-            robot.compute_quadrant()
+fn print_room(dimensions: &Vector, robots: &[Robot]) {
+    let room = (0..dimensions.y)
+        .map(move |y| {
+            (0..dimensions.x)
+                .map(move |x| {
+                    let pos = Vector { x, y };
+                    let any = robots.iter().any(|robot| robot.position == pos);
+                    lazy_format!(
+                        if any => "O"
+                        else => "."
+                    )
+                })
+                .join_concat()
         })
-        .collect();
+        .join_with(Newline);
 
-    // This will be wrong if any quadrant is empty, since counters skip those.
-    Ok(robot_counts.iter().map(|(_, count)| count.get()).product())
+    eprintln!("{}", room);
 }
 
-pub fn part2(input: Input) -> anyhow::Result<Infallible> {
-    anyhow::bail!("not implemented yet")
+const DEFAULT_ROOM_HEIGHT: i64 = 103;
+const DEFAULT_ROOM_WIDTH: i64 = 101;
+
+const DEFAULT_ROOM: Vector = Vector {
+    x: DEFAULT_ROOM_WIDTH,
+    y: DEFAULT_ROOM_HEIGHT,
+};
+
+pub fn part1(mut input: Input) -> anyhow::Result<usize> {
+    eprintln!("got {} robots", input.robots.len());
+    let room = get_env_room()?.unwrap_or(DEFAULT_ROOM);
+
+    input
+        .robots
+        .iter_mut()
+        .for_each(|robot| robot.take_steps(100, &room));
+
+    print_room(&room, &input.robots);
+
+    let robot_counts: EnumCounter<Quadrant> = input
+        .robots
+        .iter()
+        .filter_map(|robot| robot.compute_quadrant(&room))
+        .collect();
+
+    eprintln!(
+        "got {} robots in quadrants",
+        robot_counts
+            .iter()
+            .map(|(_, count)| count.get())
+            .sum::<usize>()
+    );
+
+    // This will be wrong if any quadrant is empty, since counters skip those.
+    Ok(dbg!(robot_counts)
+        .iter()
+        .map(|(_, count)| count.get())
+        .product())
+}
+
+pub fn part2(mut input: Input) -> anyhow::Result<Infallible> {
+    let room = get_env_room()?.unwrap_or(DEFAULT_ROOM);
+
+    for tick in 1.. {
+        input
+            .robots
+            .iter_mut()
+            .for_each(|robot| robot.take_steps(1, &room));
+
+        print_room(&room, &input.robots);
+        eprintln!("{tick}");
+
+        sleep(Duration::from_millis(100));
+    }
+
+    anyhow::bail!("infinite loop wasn't infinite")
 }
